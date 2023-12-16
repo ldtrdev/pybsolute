d75d01634382aa588aeb4f2b4bec35293bc8e77c
LDTR <108385721+ldtrdev@users.noreply.github.com>
2023-12-15 21:45:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='pybsolute',
+    version='0.0.1',
+    packages=find_packages(),
+    author='Luis David Trejos Rojas (ldtrdev)',
+    author_email='luis.trejos@utp.edu.co',
+    description='Your absolute Python library for (almost) everything you need',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ldtrdev/pybsolute',
+)
